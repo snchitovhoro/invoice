c766aa89d90d69722a71b08d056f40610a83b36c
snchitovhoro <snchitovhoro@gmail.com>
2018-08-18 11:29:20 +0200

    Added model entities

---
+++ src/main/java/za/co/digitalplatoon/invoiceservice/invoice/model/Invoice.java
@@ -0,0 +1,94 @@
+package za.co.digitalplatoon.invoiceservice.invoice.model;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    @NotNull
+    private String client;
+
+    @NotNull
+    private Long vatRate;
+
+    private LocalDateTime invoiceDate = LocalDateTime.now();
+
+    @OneToMany(targetEntity = LineItem.class, mappedBy = "invoice", fetch = FetchType.EAGER)
+    @Cascade({CascadeType.SAVE_UPDATE})
+    @Column
+    private Set<LineItem> lineItems = new HashSet<>();
+
+    public Invoice() {
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public Long getVatRate() {
+        return vatRate;
+    }
+
+    public void setVatRate(Long vatRate) {
+        this.vatRate = vatRate;
+    }
+
+    public LocalDateTime getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(LocalDateTime invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    //@OneToMany(targetEntity = LineItem.class, fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    public Set<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public void setLineItems(Set<LineItem> lineItems) {
+
+        this.lineItems.addAll(lineItems);
+    }
+
+    public BigDecimal getSubtotal(){
+        BigDecimal invoiveTotal = new BigDecimal(0).setScale(2,BigDecimal.ROUND_HALF_UP);
+        for (LineItem lineItem: getLineItems()) {
+            invoiveTotal.add(lineItem.getLineItemTotal());
+        }
+        return invoiveTotal;
+    }
+
+    public BigDecimal getVat(){
+        BigDecimal vatTotal = new BigDecimal(0).setScale(2,BigDecimal.ROUND_HALF_UP);
+        for (LineItem lineItem: getLineItems()) {
+            vatTotal.add(lineItem.getLineItemTotal().multiply(new BigDecimal(vatRate.toString())));
+        }
+        return vatTotal.divide(new BigDecimal(100).setScale(2,BigDecimal.ROUND_HALF_UP));
+    }
+
+    public BigDecimal getTotal(){
+        return getSubtotal().add(getVat()).setScale(2,BigDecimal.ROUND_HALF_UP);
+    }
+
+
+}
---
+++ src/main/java/za/co/digitalplatoon/invoiceservice/invoice/model/LineItem.java
@@ -0,0 +1,69 @@
+package za.co.digitalplatoon.invoiceservice.invoice.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+@Entity
+public class LineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotNull
+    private Long quantity;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private BigDecimal unitPrice;
+
+    @ManyToOne
+    private Invoice invoice;
+
+    public LineItem() {
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+
+    public BigDecimal getLineItemTotal(){
+        
+        return unitPrice.multiply(new BigDecimal(quantity.toString()).setScale(2,BigDecimal.ROUND_HALF_UP));        
+    }
+
+}
